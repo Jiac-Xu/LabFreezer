d7e6f14414336f1f43d555b557118d86eae7e039
Jiac-Xu <Jiac-Xu@users.noreply.github.com>
2026-06-25 11:09:55 +0800

    🔖 升级版本至 1.2.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.labfreezer"
         minSdk = 26
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.2.4"
+        versionCode = 8
+        versionName = "1.2.5"
     }
 
     signingConfigs {
