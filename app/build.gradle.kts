ebb138e6fda180b67447c831dfe2284c5b75071b
Jiac-Xu <Jiac-Xu@users.noreply.github.com>
2026-07-19 20:04:49 +0800

    chore: bump version to 1.2.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.labfreezer"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.2.7"
+        versionCode = 11
+        versionName = "1.2.8"
     }
 
     signingConfigs {
